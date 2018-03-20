8fa1bcf017ba16c8733cfbee90e48f747904e8a0
minji7257 <kimmj.95@gmail.com>
2018-03-21 00:22:35 +0900

    geth connected

--- app/src/main/java/com/kimmj/ardapp/Account.java
+++
@@ -1,7 +0,0 @@
-package com.kimmj.ardapp;
-
-/**
- * Created by user on 2018-03-11.
- */
-public class Account {
-}
---
+++ app/src/main/java/com/kimmj/ardapp/DMapAccount.java
@@ -0,0 +1,11 @@
+package com.kimmj.ardapp;
+
+import org.ethereum.geth.KeyStore;
+
+/**
+ * class DMapAccount
+ * store the user's account information
+ */
+public class DMapAccount {
+
+}
